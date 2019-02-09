633c20e436df9eb64870b77faa6c51eb258cabe8
JZDesign <jacobzivandesign@gmail.com>
2019-02-09 10:15:06 -0600

    more refactors

--- src/bookstore/controller/CardBuilder.kt
+++ src/bookstore/controller/CardBuilder.kt
@@ -5,37 +5,33 @@ class CardBuilder {
 
     fun build(book: Book) = Card(div(), div(), div(), div(), img(), button()).let { card ->
         card.apply {
-            bind(book, card)
-            applyStyle(card)
+            bind(book)
+            applyStyle()
             container.append(image, title, description, price, button)
         }
         card.container
     }
 
-    private fun bind(book: Book, card: Card) {
-        card.apply {
-            image.src = book.coverUrl
-            title.innerHTML = book.title
-            price.innerHTML = book.price
-            description.innerHTML = book.description
-            button.apply {
-                innerHTML = BUTTON_TEXT
-                // @formatter:off
+    private fun Card.bind(book: Book) = apply {
+        image.src = book.coverUrl
+        title.innerHTML = book.title
+        price.innerHTML = book.price
+        description.innerHTML = book.description
+        button.apply {
+            innerHTML = BUTTON_TEXT
+            // @formatter:off
             addEventListener(CLICK, { window.open(book.url) })
             // @formatter:on
-            }
         }
     }
 
-    private fun applyStyle(card: Card) {
-        card.apply {
-            container.addClass(CARD, CARD_SHADOW)
-            image.addClass(COVER_IMAGE)
-            title.addClass(TITLE, FLOAT_LEFT)
-            description.addClass(DESCRIPTION, FLOAT_LEFT)
-            price.addClass(PRICE, FLOAT_LEFT)
-            button.addClass(DETAILS, RIPPLE, FLOAT_RIGHT)
-        }
+    private fun Card.applyStyle() = apply {
+        container.addClass(CARD, CARD_SHADOW)
+        image.addClass(COVER_IMAGE)
+        title.addClass(TITLE, FLOAT_LEFT)
+        description.addClass(DESCRIPTION, FLOAT_LEFT)
+        price.addClass(PRICE, FLOAT_LEFT)
+        button.addClass(DETAILS, RIPPLE, FLOAT_RIGHT)
     }
 
 }
